f00d2a505538d1fb35b384f88a9f2d7dc41edbce
Giovanni <giovanni.fioretto.23@hotmail.it>
2025-10-15 09:50:25 +0200

    Initial Commit

---
+++ AmazingProjectWithTCA/AmazingProjectWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmazingProjectWithTCAApp.swift
+//  AmazingProjectWithTCA
+//
+//  Created by Giovanni Fioretto on 15/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AmazingProjectWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
